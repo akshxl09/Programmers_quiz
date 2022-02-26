0b7dd0228865cc500d25f6eabb0be0949fe5c6ca
akshxl09 <59478929+akshxl09@users.noreply.github.com>
2022-02-26 23:30:47 +0900

    2022-02-26

--- python/LEVEL 2/피로도.py
+++ python/LEVEL 2/피로도.py
@@ -1,6 +1,5 @@
+"""
 from itertools import permutations
-
-from pkg_resources import ResolutionError
 def solution(k, dungeons):
 
     result = 0
@@ -15,6 +14,26 @@ def solution(k, dungeons):
             result = cnt
     
     return result
+"""
+answer = 0
+def dfs(k, dungeons, visited, cnt):
+    global answer
+    if k <= 0: return
+    if cnt > answer: 
+        answer = cnt
+
+    for i in range(len(dungeons)):
+        if not visited[i] and k >= dungeons[i][0]:
+            visited[i] = 1
+            dfs(k-dungeons[i][1], dungeons, visited, cnt+1)
+            visited[i] = 0
+
+def solution(k, dungeons):
+    visited = [0] * len(dungeons)
+    dfs(k, dungeons, visited, 0)
+
+    return answer 
+
 
 
 print(solution(80,[[80,20],[50,40],[30,10],[60,30],[20,10],[30,20]]))
